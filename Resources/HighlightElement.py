74ca08063f44ead2e20db8f6bb8daca9db11c3c5
Tautvydas-coder <b7&*%13kHqvP>
2022-09-05 16:55:06 +0300

    refactored function

--- Resources/HighlightElement.py
+++ Resources/HighlightElement.py
@@ -15,6 +15,9 @@ driver.get(URL)
 def highlight():
     # Highlights a Selenium Webdriver element
     element = driver.find_element(By.XPATH, ACCEPT_COOKIES)
+    print(element)
+    # session and element strings always have differens values
+    # <selenium.webdriver.remote.webelement.WebElement (session="5b63ae06a9909947d722318bb9fae8b0", element="3e7f0497-aa1a-4b4e-a980-e3c0d84d94d0")>
     driver.execute_script("arguments[0].setAttribute('style', arguments[1]);", element, "border: 5px solid red;")
     return True
 
